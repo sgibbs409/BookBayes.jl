cabda38ffd33f205947a0ebb49ac574967663e00
sgibbs409 <s>
2021-01-17 18:57:53 -0800

    Moved verbose tests from src to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,40 @@
 using Test, BookBayes
 
-#out = plusTwo(3)
+
+
+
+X = Variable(:x, 2)
+Y = Variable(:y, 2)
+Z = Variable(:z, 2)
+
+ϕ = Factor([X, Y, Z], FactorTable(
+    (x=1, y=1, z=1) => 0.08,
+    (x=1, y=1, z=2) => 0.31,
+    (x=1, y=2, z=1) => 0.09,
+    (x=1, y=2, z=2) => 0.37,
+    (x=2, y=1, z=1) => 0.01,
+    (x=2, y=1, z=2) => 0.05,
+    (x=2, y=2, z=1) => 0.02,
+    (x=2, y=2, z=2) => 0.07,
+))
+
+
+
+
+
+k = assignments(ϕ.vars)
+@show k
+@show k[2,1,1]
+@show k[2,1,1].x
+@show k[2,1,1].y
+@show k[2,1,1].z
+
+
+display(ϕ.table)
+
+display(ϕ.vars)
+
+display(variablenames(ϕ))
+
 
 @test true
