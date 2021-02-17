a4c2ee2300e256e8ac6f39af62e96c8ec6d0d49d
sgibbs409 <asdfa@asdd.com>
2021-02-17 14:29:52 -0800

    Added more to ch11 api.

--- src/ch11_PolicyGradientEstimation.jl
+++ src/ch11_PolicyGradientEstimation.jl
@@ -3,7 +3,9 @@
 # -------------- CH 11: POLICY GRADIENT ESTIMATION --------------
 
 
-export  simulate
+export  simulate,
+        FiniteDifferenceGradient,
+        gradient
 
 """
     function simulate(𝒫::MDP, s, π, d)
@@ -25,4 +27,49 @@ function simulate(𝒫::MDP, s, π, d)
     end
     # return record of state-action-reward tuples
     return τ
+end
+
+
+
+
+
+
+"""
+    struct FiniteDifferenceGradient
+        𝒫 # problem
+        b # initial state distribution
+        d # depth
+        m # number of samples
+        δ # step size
+    end
+"""
+struct FiniteDifferenceGradient
+    𝒫 # problem
+    b # initial state distribution
+    d # depth
+    m # number of samples
+    δ # step size
+end
+
+
+@doc raw"""
+    function gradient(M::FiniteDifferenceGradient, π, θ)
+
+Calculate policy gradient for policy `π` at parameterization `θ`, with respect to `θ`.
+
+Returns: $\del_{\theta} U(\pi_{\theta})$
+"""
+function gradient(M::FiniteDifferenceGradient, π, θ)
+
+    𝒫, b, d, m, δ, γ, n = M.𝒫, M.b, M.d, M.m, M.δ, M.𝒫.γ, length(θ)
+
+    Δθ(i) = [i == k ? δ : 0.0 for k in 1:n]
+
+    R(τ) = sum(r*γ^(k-1) for (k, (s,a,r)) in enumerate(τ))
+
+    U(θ) = mean(R(simulate(𝒫, rand(b), s->π(θ, s), d)) for i in 1:m)
+
+    ΔU = [U(θ + Δθ(i)) - U(θ) for i in 1:n]
+
+    return ΔU ./ δ
 end
